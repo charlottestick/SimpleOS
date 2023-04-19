b7768d910f315d0d3fc1399c9f0f86abb9b1ef48
Charlotte Stick <75076090+charlottestick@users.noreply.github.com>
2023-04-19 10:27:46 +0100

    Extend compile script to take addressOffset param

--- compile.sh
+++ compile.sh
@@ -5,14 +5,22 @@
 
 # Could also pass the output filename as parameter
 
+# Usage: ./compile.sh filename.c addressOffset
+
 sourceFileName=$1
+addressOffset=$2
 stringLength=${#sourceFileName}
+
+if [ ${#addressOffset} -lt 1]; then
+    addressOffset=0x0
+fi
+
 if [ ${stringLength} -gt 0 ]; then # Check argument is passed
     if [ ${sourceFileName:${stringLength}-2} = '.c' ]; then # Check file extension is .c
         objectFileName="${sourceFileName:0:${stringLength}-2}.o"
         binFileName="${sourceFileName:0:${stringLength}-2}.bin"
         gcc -target i386-none-elf -ffreestanding -c ${sourceFileName} -o ${objectFileName}
-        gobjcopy -O binary --change-section-address ".text"=0x0 ${objectFileName} ${binFileName}
+        gobjcopy -O binary --change-section-address ".text"=${addressOffset} ${objectFileName} ${binFileName}
     else
         echo 'Pass a C source code file to compile to raw binary'
         exit 1
